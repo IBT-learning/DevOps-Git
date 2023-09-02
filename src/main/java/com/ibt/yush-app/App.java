0bd2c5e94952064b924c7b2d45c196075a363db8
Yusha77 <141201860+Yusha77@users.noreply.github.com>
2023-09-02 18:11:31 -0400

    Add files via upload

---
+++ src/main/java/com/ibt/yush-app/App.java
@@ -0,0 +1,13 @@
+package com.ibt.yush_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
